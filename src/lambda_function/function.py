b04eda66e731c46408eee1676d80049deb13d577
Joseph Wortmann <joseph.wortmann@gmail.com>
2021-05-20 12:45:03 -0500

    2.7 -> 3.8

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -1,85 +1,93 @@
-import boto3
-import decimal
 import json
-import logging.config
-import os
+from decimal import Decimal
+from logging import INFO, getLogger
+from os import environ
+from typing import Any, Dict, List
 
+from boto3 import client
 from boto3.dynamodb.types import Binary, TypeDeserializer
 
-delivery_stream_name = os.environ['DELIVERY_STREAM_NAME']
-dynamodb_image_type = os.environ.get('DYNAMNODB_IMAGE_TYPE', 'NewImage')
-firehose = boto3.client('firehose')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+DELIVERY_STREAM_NAME = environ["DELIVERY_STREAM_NAME"]
+DYNAMODB_IMAGE_TYPE = environ.get("DYNAMNODB_IMAGE_TYPE", "NewImage")
+FIREHOSE = client("firehose")
+getLogger().setLevel(environ.get("LOGGING_LEVEL") or INFO)
 
 
 class DynamoDBEncoder(json.JSONEncoder):
     def default(self, value):
-        if isinstance(value, decimal.Decimal):
+        if isinstance(value, Decimal):
             return float(value)
         elif isinstance(value, Binary):
             return bytes(value)
-        return super(DynamoDBEncoder, self).default(value)
+        return super().default(value)
 
 
-class DynamoDBTypeDeserializer(TypeDeserializer):
-    def _deserialize_b(self, value):
-        if isinstance(value, unicode):
-            value = str(value)
-        return super(DynamoDBTypeDeserializer, self)._deserialize_b(value)
+DESERIALIZER = TypeDeserializer()
 
 
-type_deserializer = DynamoDBTypeDeserializer()
+def handler(event: Dict[str, Any], context):
 
+    getLogger().debug("Processing event {}".format(json.dumps(event)))
 
-def handler(event, context):
-
-    logger.debug('Processing event {}'.format(json.dumps(event)))
-
-    if 'Records' in event and len(event['Records']):
-        map(put_records_batch, create_kinesis_batches(event['Records']))
+    if (records := event.get("Records")) and len(records):
+        map(put_records_batch, create_kinesis_batches(records))
 
     return event
 
 
-def create_kinesis_batches(dynamodb_records):
+def create_kinesis_batches(dynamodb_records) -> List[List[Dict[str, str]]]:
     if not dynamodb_records or not len(dynamodb_records):
         return []
     kinesis_records = []
     count = 0
     total_length = 0
     for dynamodb_record in dynamodb_records:
-        if dynamodb_image_type in dynamodb_record['dynamodb']:
-            image = {
-                k: type_deserializer.deserialize(v) for k, v in dynamodb_record['dynamodb'][dynamodb_image_type].items()
-            }
-            data = json.dumps(image, separators=(',', ':'), cls=DynamoDBEncoder) + '\n'
+        image: Dict[str, Any] = None
+        dynamodb: Dict[str, Any] = dynamodb_record["dynamodb"]
+        if image := dynamodb.get(DYNAMODB_IMAGE_TYPE):
+            data = (
+                json.dumps(
+                    {k: DESERIALIZER.deserialize(v) for k, v in image.items()},
+                    separators=(",", ":"),
+                    cls=DynamoDBEncoder,
+                )
+                + "\n"
+            )
             total_length += len(data)
             if total_length >= 4194304:
                 break
-            kinesis_records.append(
-                {
-                    'Data': data
-                }
-            )
+            kinesis_records.append({"Data": data})
         count += 1
         if len(kinesis_records) == 500:
             break
-    return [kinesis_records]+[create_kinesis_batches(dynamodb_records[count:])]
+    return [kinesis_records] + create_kinesis_batches(dynamodb_records[count:])
 
 
 def put_records_batch(batch):
-    if not batch or not len(batch):
+    if not batch:
         return
-    response = firehose.put_record_batch(
-        DeliveryStreamName=delivery_stream_name,
-        Records=batch
+    response = FIREHOSE.put_record_batch(
+        DeliveryStreamName=DELIVERY_STREAM_NAME, Records=batch
     )
-    logger.info('Successfully processed {} records'.format(len(batch) - response['FailedPutCount']))
-    if response['FailedPutCount']:
-        logger.warn('Failed to process {} records out of {}'.format(response['FailedPutCount'], len(batch)))
-        logger.warn(
-            'Failed requests {}'.format(
-                json.dumps([request for request in response['RequestResponses'] if 'ErrorCode' in request])
+    getLogger().info(
+        "Successfully processed {} records".format(
+            len(batch) - response["FailedPutCount"]
+        )
+    )
+    if response["FailedPutCount"]:
+        getLogger().warn(
+            "Failed to process {} records out of {}".format(
+                response["FailedPutCount"], len(batch)
+            )
+        )
+        getLogger().warn(
+            "Failed requests {}".format(
+                json.dumps(
+                    [
+                        request
+                        for request in response["RequestResponses"]
+                        if "ErrorCode" in request
+                    ]
+                )
             )
         )
